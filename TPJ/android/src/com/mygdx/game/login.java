66a12423272b33d90b8943c2e870ecaf1cc6eb03
luixmartinv2 <luixmartinv2@gmail.com>
2018-05-23 13:09:05 +0200

    Commit 2

---
+++ TPJ/android/src/com/mygdx/game/login.java
@@ -0,0 +1,14 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
