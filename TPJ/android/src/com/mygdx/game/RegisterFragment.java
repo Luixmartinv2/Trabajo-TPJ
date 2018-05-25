b915450b275f3d42e17be110e59a10dec39d7bb6
luixmartinv2 <luixmartinv2@gmail.com>
2018-05-25 10:01:30 +0200

    Commit4
    dejamos listo el registro
    sin actualizar gradel

--- TPJ/android/src/com/mygdx/game/RegisterFragment.java
+++ TPJ/android/src/com/mygdx/game/RegisterFragment.java
@@ -1,11 +1,18 @@
 package com.mygdx.game;
 
 
+
+
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -14,15 +21,60 @@ import android.view.ViewGroup;
 public class RegisterFragment extends Fragment {
 
 
+    private TextView lblRUser;
+    public EditText txtfRUser;
+    private TextView lblRPass;
+    public EditText txtfRPass;
+    public Button btnRBack;
+    public Button btnROK;
+    public RegisterFragmentListener listener;
+
+
     public RegisterFragment() {
         // Required empty public constructor
     }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+    public void setListener (RegisterFragmentListener listener){
+        this.listener = listener;
     }
 
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+        lblRUser = v.findViewById(R.id.lblRUser);
+        txtfRUser = v.findViewById(R.id.txtfRUser);
+        lblRPass = v.findViewById(R.id.lblRPass);
+        txtfRPass = v.findViewById(R.id.txtfRPass);
+        btnRBack = v.findViewById(R.id.btnRBack);
+        btnROK = v.findViewById(R.id.btnROK);
+        RegisterFragmentEvents events = new RegisterFragmentEvents(this);
+        btnRBack.setOnClickListener(events);
+        btnROK.setOnClickListener(events);
+
+        return v;
+    }
+
+}
+
+class RegisterFragmentEvents implements View.OnClickListener{
+    private RegisterFragment registerFragment;
+
+    //Se pasa la referencia por parámetro y se inicializa la variable
+    public RegisterFragmentEvents (RegisterFragment fragment){
+        this.registerFragment = fragment;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()== this.registerFragment.btnRBack.getId()){
+            this.registerFragment.listener.RegisterFragmentBackButtonClicked();
+        }else if (view.getId()== this.registerFragment.btnROK.getId()){
+            Log.v("EEE1", "oofofof"+this.registerFragment.txtfRUser.getText().toString());
+            this.registerFragment.listener.RegisterFragmentOKButtonClicked(this.registerFragment.txtfRUser.getText().toString()
+                    ,this.registerFragment.txtfRPass.getText().toString());
+        }
+    }
 }
---
+++ TPJ/android/src/com/mygdx/game/RegisterFragmentListener.java
@@ -0,0 +1,10 @@
+package com.mygdx.game;
+
+/**
+ * Created by luixmartinv2 on 23/5/18.
+ */
+
+interface RegisterFragmentListener {
+    public void RegisterFragmentBackButtonClicked();
+    public void RegisterFragmentOKButtonClicked(String sUser, String sPass);
+}
